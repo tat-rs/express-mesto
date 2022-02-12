acbc64ab23bd5e69acc8f05d3ddfaec6b08e073d
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-02-12 19:02:15 +0300

    feat: создать схемы и модели: user, card

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен через порт ${PORT}`);
