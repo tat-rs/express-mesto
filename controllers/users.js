6f4d8f14a6ca86495b110a8652a4f868cceacec8
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-02-13 17:26:36 +0300

    feat: реализовать роут обновления данных пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,11 @@
 const User = require('../models/user');
 
+const optionsOfData = {
+  new: true,
+  runValidators: true,
+  upsert: true,
+};
+
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ data: users }))
@@ -26,8 +32,38 @@ const createUser = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+const uptadeUserProfile = (req, res) => {
+  const userId = req.user._id;
+  const { name, about } = req.body;
+
+  if (req.body.name && req.body.about) {
+    User.findByIdAndUpdate(userId, { name, about }, optionsOfData)
+      .then((user) => res.status(200).send({ data: user }))
+      .catch((err) => res.status(500).send({ message: err.message }));
+  } else {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
+
+const uptadeUserAvatar = (req, res) => {
+  const userId = req.user._id;
+  const { avatar } = req.body;
+
+  if (req.body.avatar) {
+    User.findByIdAndUpdate(userId, { avatar }, optionsOfData)
+      .then((user) => {
+        res.status(200).send({ data: user });
+      })
+      .catch((err) => res.status(500).send({ message: err.message }));
+  } else {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
+  uptadeUserProfile,
+  uptadeUserAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const userRouter = express.Router();
 
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+
+  getUsers, getUserById, createUser, uptadeUserProfile, uptadeUserAvatar,
+
+} = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
 
 userRouter.get('/users/:userId', getUserById);
 
 userRouter.post('/users', express.json(), createUser);
+
+userRouter.patch('/users/me', express.json(), uptadeUserProfile);
+
+userRouter.patch('/users/me/avatar', express.json(), uptadeUserAvatar);
 
 module.exports = userRouter;
